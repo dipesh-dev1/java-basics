b898d304b1f79fd5b3e98bef7838b0f08cf1cf63
Dipesh Chaudhary <chaudharydipesh190@gmail.com>
2025-10-18 14:42:46 +0545

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.learningphase;
+
+public class Test {
+    public static void main(String[] args) {
+       //Implicit Type Casting
+        int h = 9;
+        double a = h;
+
+        System.out.println(a);
+    }
+}
